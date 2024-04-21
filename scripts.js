a253e902171d06232a897039c0d5392e8c1453dd
apoorva.ce <apoorva.ce@subex.com>
2024-04-21 17:09:51 +0530

    a typeahead/search implementation using JS

---
+++ scripts.js
@@ -0,0 +1,35 @@
+let availableKeywords = [
+    'HTML',
+    'CSS',
+    'JAVASCRIPT',
+    'DIV',
+    'ELEMENT'
+];
+
+var resultbox = document.querySelector("#result-box");
+var inputBox = document.getElementById("input-box");
+
+let timeoutId;
+
+inputBox.addEventListener('input', (e) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {  //DEBOUNCING
+        const input = e.target.value.toLowerCase();
+        const result = input ? availableKeywords.filter(data => data.toLowerCase().includes(input)) : [];
+        display(result);
+    }, 500);
+})
+
+function display(result) {
+    console.log("value");
+    const content = result.map((list) => {
+        return "<li>" + list + "</li>";
+    })
+    if (result.length > 0) {
+        resultbox.classList.add("result-box");
+    }
+    else {
+        resultbox.classList.remove("result-box");
+    }
+    resultbox.innerHTML = "<ul>" + content + "</ul>";
+}
